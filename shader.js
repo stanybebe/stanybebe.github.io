f7ca6d488ca1a27c09c5765b1d91430dc8c0f4c2
Stany Bebe <tristan.e.whitehill@gmail.com>
2019-11-30 20:58:45 -0500

    Add files via upload

---
+++ shader.js
@@ -0,0 +1,22 @@
+var canvasDiv = document.getElementById('myCanvas');
+var width = splash.clientWidth;
+function preload() {
+  // note that we are using two instances
+  // of the same vertex and fragment shaders
+  redGreen = loadShader('bkg.vert','bkg.frag');
+}
+
+function setup() {
+  createCanvas(width, 230, WEBGL);
+ var sketchCanvas = createCanvas(width,450);
+ sketchCanvas.parent("myCanvas");
+
+}
+
+function draw() {
+  shader(redGreen);
+  redGreen.setUniform("u_resolution", [width, height]);
+  redGreen.setUniform("u_time", millis() / 1000.0);
+  rect(0,0,width, height);
+  console.log('2');
+}
