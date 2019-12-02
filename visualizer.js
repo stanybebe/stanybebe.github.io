e23b16aa5f3355d3353d4adf65c52e8eb5b95ba9
stanybebe <tristan.e.whitehill@gmail.com>
2019-12-01 21:35:11 -0500

    Update visualizer.js

--- visualizer.js
+++ visualizer.js
@@ -11,7 +11,10 @@ function draw() {
 background(200,0,255);
 rotateX(frameCount*.01);
 if( note == 48) {
-scale(100);
+let c = color(255, 204, 0); // Define color 'c'
+fill(c); // Use color variable 'c' as fill color
+noStroke(); // Don't draw a stroke around shapes
+scale(10);
 model(orbo);
 note=0;
 }
