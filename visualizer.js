888489bb27157d2321920974edbcff0de1a628de
stanybebe <tristan.e.whitehill@gmail.com>
2019-12-01 21:38:02 -0500

    Update visualizer.js

--- visualizer.js
+++ visualizer.js
@@ -1,4 +1,5 @@
 let orbo;
+var timer=60;
 function preload() {
 orbo= loadModel('assets/orbo.obj');
 }
@@ -10,13 +11,19 @@ function setup() {
 function draw() {
 background(200,0,255);
 rotateX(frameCount*.01);
+timer--;
 if( note == 48) {
+  if(timer>0){
 let c = color(255, 204, 0); // Define color 'c'
 fill(c); // Use color variable 'c' as fill color
 noStroke(); // Don't draw a stroke around shapes
 scale(10);
 model(orbo);
+}else{timer=60;
 note=0;
+
+}
+
 }
 
 
