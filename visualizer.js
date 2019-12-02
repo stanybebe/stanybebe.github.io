68e823d3d2b273fab6c8eac2c25908d74a5fd61c
stanybebe <tristan.e.whitehill@gmail.com>
2019-12-01 21:19:40 -0500

    Update visualizer.js

--- visualizer.js
+++ visualizer.js
@@ -1,7 +1,7 @@
-//let orbo;
-//function preload() {
-//orbo= loadModel('https://github.com/stanybebe/stanybebe.github.io/blob/master/assets/orbo.obj');
-//}
+let orbo;
+function preload() {
+orbo= loadModel('assets/orbo.obj');
+}
 
 function setup() {
  createCanvas(700, 700,WEBGL);
@@ -9,12 +9,12 @@ function setup() {
 
 function draw() {
 background(200,0,255);
-rotateX(frameRate*.001);
+rotateX(frameCount*.01);
 if( note == 48) {
-box(100);
-
-}else {
-  note=0;
+size(100);
+model(orbo);
+note=0;
 }
+
 
 }
