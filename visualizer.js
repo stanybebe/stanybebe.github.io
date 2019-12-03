5c54538b8f50cc6c33948756ade5e8d9081d9113
stanybebe <tristan.e.whitehill@gmail.com>
2019-12-03 16:48:51 -0500

    Update visualizer.js

--- visualizer.js
+++ visualizer.js
@@ -40,7 +40,7 @@ if( note == 0 || note == 12 || note == 24 || note == 36 || note==48 || note == 6
   if(timer>0){
 push();
 rotateX(90);
-let c = color(255,208,175); // Define color 'c'
+let c = color(254, 111, 76); // Define color 'c'
 fill(c); // Use color variable 'c' as fill color
 noStroke(); // Don't draw a stroke around shapes
 scale(140);
@@ -56,7 +56,7 @@ if( note == 1 || note == 13 || note == 25 || note == 37 || note==49 || note == 6
   if(timer>0){
 push();
 rotateX(90);
-let c = color(255,208,175); // Define color 'c'
+let c = color(255,76,108); // Define color 'c'
 fill(c); // Use color variable 'c' as fill color
 noStroke(); // Don't draw a stroke around shapes
 scale(140);
@@ -73,7 +73,7 @@ if( note == 2 || note == 14 || note == 26 || note == 38 || note==50 || note == 6
   if(timer>0){
 push();
 rotateX(90);
-let c = color(255,208,175); // Define color 'c'
+let c = color(255, 161, 177); // Define color 'c'
 fill(c); // Use color variable 'c' as fill color
 noStroke(); // Don't draw a stroke around shapes
 scale(140);
@@ -89,7 +89,7 @@ if( note == 3 || note == 15 || note == 27 || note == 39 || note==51 || note == 6
   if(timer>0){
 push();
 rotateX(90);
-let c = color(255,208,175); // Define color 'c'
+let c = color(255, 179, 161); // Define color 'c'
 fill(c); // Use color variable 'c' as fill color
 noStroke(); // Don't draw a stroke around shapes
 scale(140);
@@ -105,7 +105,7 @@ if( note == 4 || note == 16 || note == 28 || note == 40 || note==52 || note == 6
   if(timer>0){
 push();
 rotateX(90);
-let c = color(255,208,175); // Define color 'c'
+let c = color(16, 209, 34); // Define color 'c'
 fill(c); // Use color variable 'c' as fill color
 noStroke(); // Don't draw a stroke around shapes
 scale(140);
@@ -121,7 +121,7 @@ if( note == 5 || note == 17 || note == 29 || note == 41 || note==53 || note == 6
   if(timer>0){
 push();
 rotateX(90);
-let c = color(255,208,175); // Define color 'c'
+let c = color(85, 208, 143); // Define color 'c'
 fill(c); // Use color variable 'c' as fill color
 noStroke(); // Don't draw a stroke around shapes
 scale(140);
@@ -137,7 +137,7 @@ if( note == 6 || note == 18 || note == 30 || note == 42 || note==54 || note == 6
   if(timer>0){
 push();
 rotateX(90);
-let c = color(255,208,175); // Define color 'c'
+let c = color(51, 255, 147); // Define color 'c'
 fill(c); // Use color variable 'c' as fill color
 noStroke(); // Don't draw a stroke around shapes
 scale(140);
@@ -153,7 +153,7 @@ if( note == 7 || note == 19 || note == 31 || note == 43 || note==55 || note == 6
   if(timer>0){
 push();
 rotateX(90);
-let c = color(255,208,175); // Define color 'c'
+let c = color(102, 204, 150); // Define color 'c'
 fill(c); // Use color variable 'c' as fill color
 noStroke(); // Don't draw a stroke around shapes
 scale(140);
@@ -169,7 +169,7 @@ if( note == 8 || note == 20 || note == 32 || note == 44 || note==56 || note == 6
   if(timer>0){
 push();
 rotateX(90);
-let c = color(255,208,175); // Define color 'c'
+let c = color(0, 105, 209); // Define color 'c'
 fill(c); // Use color variable 'c' as fill color
 noStroke(); // Don't draw a stroke around shapes
 scale(140);
@@ -185,7 +185,7 @@ if( note == 9 || note == 21 || note == 33 || note == 45 || note==57 || note == 6
   if(timer>0){
 push();
 rotateX(90);
-let c = color(255,208,175); // Define color 'c'
+let c = color(69, 139, 209); // Define color 'c'
 fill(c); // Use color variable 'c' as fill color
 noStroke(); // Don't draw a stroke around shapes
 scale(140);
@@ -201,7 +201,7 @@ if( note == 10 || note == 22 || note == 34 || note == 46 || note==58 || note == 
   if(timer>0){
 push();
 rotateX(90);
-let c = color(255,208,175); // Define color 'c'
+let c = color(76, 165, 178); // Define color 'c'
 fill(c); // Use color variable 'c' as fill color
 noStroke(); // Don't draw a stroke around shapes
 scale(140);
@@ -218,7 +218,7 @@ if( note == 11 || note == 23 || note == 35 || note == 47 || note==59 || note == 
   if(timer>0){
 push();
 rotateX(90);
-let c = color(255,208,175); // Define color 'c'
+let c = color(16, 35, 177); // Define color 'c'
 fill(c); // Use color variable 'c' as fill color
 noStroke(); // Don't draw a stroke around shapes
 scale(140);
