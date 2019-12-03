8f1a435b1258bff588e4524b47b7e6631079c845
stanybebe <tristan.e.whitehill@gmail.com>
2019-12-03 17:59:17 -0500

    Update visualizer.js

--- visualizer.js
+++ visualizer.js
@@ -24,7 +24,7 @@ bnote= loadModel('assets/b.obj');
 function setup() {
  var cnv = createCanvas(700, 700,WEBGL);
  var x = (windowWidth - width) / 2;
-var y = (windowHeight - height) / 2;
+ var y = (windowHeight - height) / 2;
  cnv.position(x, y);
  smooth();
 }
