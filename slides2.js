5470fec80e261cc21ada4058717996c85c44d347
stanybebe <tristan.e.whitehill@gmail.com>
2020-11-24 16:08:44 -0500

    another section

---
+++ slides2.js
@@ -0,0 +1,28 @@
+var slideIndex = 1;
+showSlides1(slideIndex);
+
+// Next/previous controls
+function plusSlides1(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide1(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides1");
+  var dots = document.getElementsByClassName("dot1");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
