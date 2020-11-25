9257ed3730f024297b2474bce2ad98aad5f2e676
stanybebe <tristan.e.whitehill@gmail.com>
2020-11-25 17:53:52 -0500

    jazz

---
+++ slides3.js
@@ -0,0 +1,28 @@
+var slideIndex = 1;
+showSlides2(slideIndex);
+
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides1(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides2(slideIndex = n);
+}
+
+function showSlides2(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("dot2");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
