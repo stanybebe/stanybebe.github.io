72a17203488a4fda2ddf2487c22f2e40c053ebe5
stanybebe <tristan.e.whitehill@gmail.com>
2020-11-27 16:56:29 -0500

    sliders

--- slides2.js
+++ slides2.js
@@ -1,28 +1,28 @@
-var slideIndex = 1;
-showSlides1(slideIndex);
+var slideIndexb = 1;
+showSlides1(slideIndexb);
 
 // Next/previous controls
 function plusSlides1(n) {
-  showSlides1(slideIndex += n);
+  showSlides1(slideIndexb += n);
 }
 
 // Thumbnail image controls
 function currentSlide1(n) {
-  showSlides1(slideIndex = n);
+  showSlides1(slideIndexb = n);
 }
 
 function showSlides1(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides1");
   var dots = document.getElementsByClassName("dot1");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {slideIndexb = 1}
+  if (n < 1) {slideIndexb = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndexb-1].style.display = "block";
+  dots[slideIndexb-1].className += " active";
 }
