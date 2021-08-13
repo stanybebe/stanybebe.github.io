7e2d021438416b72034fffe733e8a9d78c04ed00
stanybebe <tristan.e.whitehill@gmail.com>
2021-08-13 13:41:31 -0400

    updates

--- covidMelody.js
+++ covidMelody.js
@@ -1,4 +1,6 @@
 let data = {};
+let font,
+  fontsize = 32;
 
 var midiNotes = [   'C4', 'D4', 'Eb4',  'F4',  'G4',  'A4',  'Cb5',
                     'C5', 'D5', 'Eb5',  'F5',  'G5',  'A5',  'Cb6',
@@ -8,6 +10,9 @@ var midiNotes = [   'C4', 'D4', 'Eb4',  'F4',  'G4',  'A4',  'Cb5',
                     'C9', 'D9', 'Eb9',  'F9', 'G9'];
 function preload(){
  data = loadJSON('https://raw.githubusercontent.com/stanybebe/stanybebe.github.io/master/covid.json');
+ font = loadFont('assets/EB.ttf');
+}
+
 }
 
 function setup(){
@@ -17,6 +22,9 @@ var width = myCanvas2.offsetWidth;
 
 var sketchCanvas = createCanvas(width,500, WEBGL);
 sketchCanvas.parent("myCanvas2");
+textFont(font);
+textSize(fontsize);
+textAlign(CENTER, CENTER);
 
 }
 
@@ -28,25 +36,47 @@ var mapRange = function(from, to, s) {
 
 
 function draw(){
+  background(50,150,255);
+
+
   document.querySelector('button')?.addEventListener('click', async () => {
 	await Tone.start()
 	console.log('audio is ready')
 })
+
+
 const sampler = new Tone.Sampler({
 urls: {
   "C4": "pluck.mp3"
 },
-release: 1,
-baseUrl: "https://github.com/stanybebe/stanybebe.github.io/",
-}).toDestination();
+attack:3,
+release: 5,
+baseUrl: "https://tristanwhitehill.com/audio/",
+});
 
 const sampler2 = new Tone.Sampler({
 urls: {
   "C4": "violin.mp3"
 },
+attack:7,
 release: 1,
-baseUrl: "https://github.com/stanybebe/stanybebe.github.io/",
-}).toDestination();
+baseUrl: "https://tristanwhitehill.com/audio/",
+});
+
+const sampler3 = new Tone.Sampler({
+urls: {
+  "C4": "pluck2.mp3"
+},
+attack:3,
+release:5,
+baseUrl: "https://tristanwhitehill.com/audio/",
+});
+
+const freeverb = new Tone.FeedbackDelay("8n", 0.7).toDestination();
+
+sampler.connect(freeverb);
+sampler2.connect(freeverb);
+sampler3.connect(freeverb);
 
   const synth = new Tone.Synth().toDestination();
   const synth2 = new Tone.Synth().toDestination();
@@ -58,45 +88,97 @@ baseUrl: "https://github.com/stanybebe/stanybebe.github.io/",
 //   }
  var d = [];
  var d2 = [];
+ var d3 = [];
  var mN = [];
  var mN2 = [];
+ var mN3 = [];
  console.log(d2);
   for(k in data){
 
     d.push(Number(data[k].deathIncrease));
     d2.push(Number(data[k].positiveIncrease));
+    d3.push(Number(data[k].inIcuCumulative));
   }
   var invertd = d.reverse();
   var invertd2 = d2.reverse();
+  var invertd3 = d3.reverse();
 
   for (var i = 0; i < invertd.length; i++) {
     invertd[i] = mapRange([0, 10000], [0, 24], invertd[i]);
     invertd2[i] = mapRange([0, 600000], [0, 24], invertd2[i]);
+    invertd3[i] = mapRange([0, 60000], [0, 24], invertd3[i]);
   }
   for(n in invertd){
     mN.push(midiNotes[round(invertd[n])]);
     mN2.push(midiNotes[round(invertd2[n])]);
+    mN3.push(midiNotes[round(invertd3[n])]);
   }
 
  console.log(d);
  console.log(mN);
  console.log(d2);
  console.log(mN2);
-  console.log(midiNotes.length);
+ console.log(midiNotes.length);
 
   const loopA = new Tone.Sequence((time,note) => {
 
     sampler.triggerAttackRelease(note, "8n",time);},mN).start(0);
   const loopB = new Tone.Sequence((time,note) => {
 
-      synth2.triggerAttackRelease(note, "4n",time);},mN2).start(0);
+    sampler2.triggerAttackRelease(note, "4n",time);},mN2).start(0);
+  const loopC = new Tone.Sequence((time,note) => {
 
+    sampler3.triggerAttackRelease(note, "4n",time);},mN3).start(0);
  Tone.Transport.start();
 
   for(j in data){
    console.log(data[j].deathIncrease);
 }
 
+push();
 
+translate(-width/2,0);
+for (var i = 0; i < invertd.length; i++){
+  let c = color(255, 0, 0);
+fill(c);
+noStroke();
+  rect(i*2.8,100,1,-invertd[i]*20);
+}
+pop()
+
+push();
+
+translate(-width/2+1,30);
+for (var i = 0; i < invertd2.length; i++){
+  let c = color(0);
+
+fill(c);
+noStroke();
+  rect(i*2.8,100,1,-invertd2[i]*20);
+}
+pop()
+
+push();
+
+translate(-width/2+2,60);
+for (var i = 0; i < invertd3.length; i++){
+
+    let c = color(255, 255, 255);
+fill(c);
+noStroke();
+  rect(i*2.8,100,1,-invertd3[i]*20);
+}
+pop()
+
+push();
+
+translate(-width/2,0);
+
+fill(c);
+noStroke();
+rect(i*2.8,100,1,-invertd3[i]*20);
+}
+pop()
  }
+ text
 // Number(data[k].deathIncrease)
